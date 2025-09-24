ddf5eb7afdb94bb952fc29f3815db6d81306dd7a
Kevin <G14460763@tdrive.thu.tw>
2025-09-24 16:52:26 +0800

    Create Max_F1.py

--- Kevin D_Sum.py
+++ Kevin D_Sum.py
@@ -1,12 +1,15 @@
 #
 # KevinD, 2025/09/24
 # File: Kevin D_Sum.py
-# Short description of the task
-#
+# Add two number then print
 
 # 1. Input
+X = 8
+Y = 7
 
 # 2. Process
+Minus = X - Y
 
 # 3. Output
-print('Hello THU')
+
+print(f'Result {Minus}')
